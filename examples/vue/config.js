4728950ac569f055a65354df5cd304d366722563
qingwei.li <cinwell.li@gmail.com>
2017-03-15 00:24:48 +0800

    fix(vue): fix publicPath

--- examples/vue/config.js
+++ examples/vue/config.js
@@ -3,5 +3,6 @@ var resolve = require('path').resolve
 module.exports = {
   entry: resolve(__dirname, './src/entry.js'),
   dist: resolve(__dirname, '../dist/vue'),
-  sourceMap: false
+  sourceMap: false,
+  homepage: 'https://wasd-org.github.io/wasd-flappy/vue/'
 }
